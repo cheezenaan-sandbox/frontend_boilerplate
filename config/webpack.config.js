d055fde0fdb4a2a37845660b839041ab9baee6f1
cheezenaan <cheezenaan@gmail.com>
2018-06-05 09:52:32 +0900

    Introduce html-webpack-plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
@@ -8,5 +9,10 @@ module.exports = {
   output: {
     path: path.join(__dirname, '../dist'),
     filename: '[name].js'
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '../src/index.html')
+    })
+  ]
 };
