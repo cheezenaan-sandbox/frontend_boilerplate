66f662f069101f2cf6a4df9fc4da3a7c37ee0d10
cheezenaan <cheezenaan@gmail.com>
2018-06-07 08:08:09 +0900

    Add settings for compiling TypeScript
    * Also create sourcemap in development env.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,18 +1,26 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   entry: {
-    bundle: './src/javascripts/index.js'
+    bundle: './src/javascripts/index.ts'
   },
   output: {
     path: path.join(__dirname, '../dist'),
     filename: '[name].js'
   },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [{ test: /.ts$/, loader: 'ts-loader' }]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, '../src/index.html')
     })
-  ]
+  ],
+  devtool: isProduction ? 'eval' : 'cheap-module-source-map'
 };
